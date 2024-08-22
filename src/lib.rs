ddde49ebe7c0d60560153932e13bd6db7e928f14
encoderuz <abduqodirxojiyev@gmail.com>
2024-08-22 18:39:23 +0500

    Library first items created

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+pub struct ByteConverter {
+    bytes: u64,
+}
+impl ByteConverter {
+    pub fn new(bytes: u64) -> Self {
+        ByteConverter { bytes }
+    }
+
+    pub fn to_kilobytes(&self) -> (f64, &'static str) {
+        (self.bytes as f64 / 1024.0, "KB")
+    }
+
+    pub fn to_megabytes(&self) -> (f64, &'static str) {
+        (self.bytes as f64 / 1024.0 / 1024.0, "MB")
+    }
+
+    pub fn to_gigabytes(&self) -> (f64, &'static str) {
+        (self.bytes as f64 / 1024.0 / 1024.0 / 1024.0, "GB")
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +25,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_conversion() {
+        let converter = ByteConverter::new(8589934592);
+        println!("{:?}", converter);
+        assert_eq!(converter.to_gigabytes().0, 8.0);
     }
 }
